209b09b893b3ecfc3bf11d2e3f28a6c54cc70399
zennzhong <zennzhong@tencent.com>
2023-05-12 14:39:19 +0800

    refresh

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -15,12 +15,14 @@ pub mod httprequest;
 use crate::{
     lua::WrkLuaVM, summary::SummaryUnit, util::transform::Transformation, CommandLineArgs,
 };
+use self::httprequest::HttpRequest;
 
 pub struct Client {
     id: (usize, usize),
     lua: Arc<WrkLuaVM>,
     client: reqwest::Client,
     summary: SummaryUnit,
+    request: HttpRequest
 }
 unsafe impl Send for Client {}
 unsafe impl Sync for Client {}
@@ -29,11 +31,13 @@ impl Client {
     pub fn new(id: (usize, usize), lua: Arc<WrkLuaVM>) -> Result<Self, mlua::Error> {
         let client = reqwest::Client::new();
         let summary = SummaryUnit::new();
+        let request = httprequest::HttpRequest::get_request(lua.get_vm()).unwrap();
         Ok(Self {
             id,
             lua,
             client,
             summary,
+            request,
         })
     }
     pub fn client_loop(
@@ -86,7 +90,7 @@ impl Client {
     }
 
     fn make_request(&mut self, args: &CommandLineArgs) -> Result<Request, mlua::Error> {
-        let request = httprequest::HttpRequest::get_request(self.lua.get_vm()).unwrap();
+        let request = self.request.clone();
 
         let method = match request.method.as_str() {
             "GET" => reqwest::Method::GET,
--- src/summary/mod.rs
+++ src/summary/mod.rs
@@ -1,11 +1,3 @@
-use std::sync::atomic::{AtomicU64, Ordering::SeqCst};
-
-use crate::{util::lazy::Lazy, LazyNew};
-
-static REQUEST_COUNT: Lazy<AtomicU64> = LazyNew!(AtomicU64::new(0));
-static TOTAL_LATENCY: Lazy<AtomicU64> = LazyNew!(AtomicU64::new(0));
-static MAX_LATENCY: Lazy<AtomicU64> = LazyNew!(AtomicU64::new(0));
-
 pub struct SummaryUnit {
     request_count: u64,
     total_latency: u64,
@@ -44,25 +36,4 @@ impl SummaryUnit {
             max_latency: self.max_latency.max(other.max_latency),
         }
     }
-}
-
-pub fn count_request() {
-    REQUEST_COUNT.fetch_add(1, SeqCst);
-}
-
-pub fn add_latency(latency: u64) {
-    MAX_LATENCY.fetch_max(latency, SeqCst);
-    TOTAL_LATENCY.fetch_add(latency, SeqCst);
-}
-
-pub fn avg_latency() -> f64 {
-    TOTAL_LATENCY.load(SeqCst) as f64 / REQUEST_COUNT.load(SeqCst) as f64
-}
-
-pub fn total_request() -> u64 {
-    REQUEST_COUNT.load(SeqCst)
-}
-
-pub fn max_latency() -> u64 {
-    MAX_LATENCY.load(SeqCst)
 }
